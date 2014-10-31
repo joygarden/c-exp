ea034f969e4802ea7bf508e9fce6eeb3fbbed061
joygarden <lzs_mymail@126.com>
2014-10-31 15:44:29 +0800

    Initial Commit

---
+++ c-exp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-exp
+//
+//  Created by liaozhisong on 10/31/14.
+//  Copyright (c) 2014 liaozhisong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
