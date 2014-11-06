0771d07dee11f051a9a12f79978857eeb52e654b
joygarden <lzs_mymail@126.com>
2014-11-06 09:52:59 +0800

    update

--- c-exp/main.c
+++ c-exp/main.c
@@ -8,8 +8,36 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int printExp() {
     printf("Hello, World!\n");
+    
+    int score = 23;
+    char c = 'b';
+    
+    printf("%d \n",score);
+    {
+        //c作用域止于大括号
+        char c = 'a';
+        printf("%d \n",c);
+    }
+
+    double d = 1.88;
+    
+    //3位小数
+    printf("%.3f \n",d);
+    
+    printf("%d %f %c %d \n",score,d,c,c);
+    
+    //控制位数3，不够补0
+    printf("%03d \n",score);
+    
     return 0;
 }
+
+
+int main(int argc, const char * argv[]) {
+    //方法定义要写在调用之前，c99规范
+    printExp();
+    return 0;
+}
+
